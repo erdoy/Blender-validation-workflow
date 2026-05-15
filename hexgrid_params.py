94f4825e8b27b1958ada6a3e610bfa7f5d81fec3
fablab <fablab@int.fablab-luebeck.de>
2026-05-15 18:28:04 +0200

    Edits made from public PC session

--- hexgrid_params.py
+++ hexgrid_params.py
@@ -221,7 +221,7 @@ class HexGridParams:
             if self.seed in df_existing["seed"].values:
                 idx = df_existing.index[df_existing["seed"] == self.seed][0]
                 for col in df_new.columns:
-                    df_existing.at[idx, col] = df_new.at[0, col]
+                    df_existing.at[idx, col] = str(df_new.at[0, col])
             else:
                 df_existing = pd.concat([df_existing, df_new], ignore_index=True)
             df_existing.to_csv(path, index=False)
